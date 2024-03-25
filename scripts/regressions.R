c23a88916bc5ff4bc829d2835d8f26e917b34198
ne1s0n <nelson.nazzicari@crea.gov.it>
2024-03-25 14:47:57 +0100

    redoing regressions, renamed rrBLUP, added Bayesian

--- scripts/regressions.R
+++ scripts/regressions.R
@@ -11,19 +11,26 @@ source(file.path(basefolder, 'scripts', 'data_manager.R'))
 outfolder = file.path(basefolder, 'results')
 dir.create(outfolder, showWarnings = FALSE, recursive = TRUE)
 
+#Bayesian regressors
+nIter = 15000
+
 # REGRESSIONS -------------------------------------------------------------
 for (trait in trait_list){
   #load data
   all_data = load_all(basefolder=file.path(basefolder, 'data'), trait=trait)
   
   #SNP based, crossvalidation
-  wb = createWorkbench(outfolder = outfolder)
+  wb = createWorkbench(outfolder = outfolder, regressor.name = 'rrBLUP')
+  wb = addRegressor(wb, regressor.name = 'BL', regressor = phenoRegressor.BGLR, type='BL', nIter = nIter)
+  wb = addRegressor(wb, regressor.name = 'BayesC', regressor = phenoRegressor.BGLR, type='BayesC', nIter = nIter)
   nds_train = createNoisyDataset(name = paste(sep='', trait, 'CVtrain'), genotypes = all_data$genos_train, phenotypes = all_data$phenos_train$phenotype_value)
   nds_test = createNoisyDataset(name = paste(sep='', trait, 'CVtest'), genotypes = all_data$genos_test, phenotypes = all_data$phenos_test$phenotype_value)
   tmp = GROAN.run(nds=nds_train, nds.test=nds_test, wb=wb)
   
   #SNP based, full set
-  wb = createWorkbench(outfolder = outfolder, folds = NULL)
+  wb = createWorkbench(outfolder = outfolder, folds = NULL, regressor.name = 'rrBLUP')
+  wb = addRegressor(wb, regressor.name = 'BL', regressor = phenoRegressor.BGLR, type='BL', nIter = nIter)
+  wb = addRegressor(wb, regressor.name = 'BayesC', regressor = phenoRegressor.BGLR, type='BayesC', nIter = nIter)
   nds_train = createNoisyDataset(name = paste(sep='', trait, 'FULLtrain'), genotypes = all_data$genos_train, phenotypes = all_data$phenos_train$phenotype_value)
   nds_test = createNoisyDataset(name = paste(sep='', trait, 'FULLtest'), genotypes = all_data$genos_test, phenotypes = all_data$phenos_test$phenotype_value)
   tmp = GROAN.run(nds=nds_train, nds.test=nds_test, wb=wb)
