8fe8aa9709670d61e62a77372267e02ee0ce86f8
ne1s0n <nelson.nazzicari@crea.gov.it>
2024-04-29 18:01:56 +0200

    new kinship computation, using the widest set of IDs

--- scripts/compute_kinship.R
+++ scripts/compute_kinship.R
@@ -1,7 +1,7 @@
 library(madbito)
 
 #go to the folder
-setwd('~/research/crea-za_2024_geno_pheno_arabidopsis/data/Cauline Leaf number (CL)')
+setwd('~/research/crea-za_2024_geno_pheno_arabidopsis/data/DaysToFlowering1/')
 
 
 # DATA LOAD ---------------------------------------------------------------
@@ -29,8 +29,8 @@ read_SNP_table = function(infile){
 }
 
 #read files
-SNP_test = read_SNP_table('X_test_705.csv.zip')
-SNP_train = read_SNP_table('X_train_705.csv.zip')
+SNP_test = read_SNP_table('X_test_703.csv.zip')
+SNP_train = read_SNP_table('X_train_703.csv.zip')
 
 #merge
 SNP = rbind(SNP_test, SNP_train)
@@ -43,7 +43,7 @@ writeLines('computing kinship')
 k = kinship.AB(data = SNP, ploidy = 1, verbose = TRUE)
 
 writeLines('saving')
-outfile = '../kinship_705.csv.gz'
+outfile = '../kinship_703.csv.gz'
 fp = gzfile(description = outfile, open = 'w')
 write.csv(k, file = fp)
 close(fp)
